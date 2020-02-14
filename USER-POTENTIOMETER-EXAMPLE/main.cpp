f2381f894107e73b93c168f3ecd647134c766b44
MCLEANS <jackagwaya@gmail.com>
2020-02-14 09:28:14 +0300

    LCD bare minimum

--- USER-POTENTIOMETER-EXAMPLE/main.cpp
+++ USER-POTENTIOMETER-EXAMPLE/main.cpp
@@ -1,9 +1,23 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include "LCD.hpp"
 
 
 int main(void){
+
+    LCD lcd;
+    lcd.initialize();
+    
+    char line_1[] = "   USER POT";
+    char line_2[] = "    EXAMPLE";
+    lcd.clear();
+    lcd.send_string(line_1);
+    lcd.new_line();
+    lcd.send_string(line_2);
+    _delay_ms(1000);
+
+
 
 
     while(1){
